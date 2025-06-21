89a5affab5e5700978403b98d60df63b4cd69b3c
Maycon Wisley <maycon.wisley@outlook.com>
2025-06-20 21:55:43 -0300

    Ajuste

--- CadeOMouse/Properties/AssemblyInfo.cs
+++ CadeOMouse/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AbrirArquivo")]
+[assembly: AssemblyTitle("Cade o Mouse")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AbrirArquivo")]
+[assembly: AssemblyProduct("Cade o Mouse")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
