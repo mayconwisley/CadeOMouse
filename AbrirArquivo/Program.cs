5d414be5061296fbae990a648b8ed9270efc8335
Maycon Wisley <maycon.wisley@outlook.com>
2025-06-20 21:30:55 -0300

    Ajuste

--- AbrirArquivo/Program.cs
+++ AbrirArquivo/Program.cs
@@ -2,48 +2,48 @@
 using System.Diagnostics;
 using System.IO;
 
-namespace AbrirArquivo
+namespace CadeOMouse
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Nenhum parâmetro de inicialização informado");
-                Console.ReadKey();
-                return;
-            }
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Nenhum parâmetro de inicialização informado");
+				Console.ReadKey();
+				return;
+			}
 
-            if (args.Length > 1)
-            {
-                Console.WriteLine("Informar apenas um parâmetro de inicialização");
-                Console.ReadKey();
-                return;
-            }
+			if (args.Length > 1)
+			{
+				Console.WriteLine("Informar apenas um parâmetro de inicialização");
+				Console.ReadKey();
+				return;
+			}
 
-            string caminhoArquivo = args[0];
+			string caminhoArquivo = args[0];
 
-            if (!File.Exists(caminhoArquivo))
-            {
-                Console.WriteLine("Arquivo não encontrado.");
-                Console.ReadKey();
-                return;
-            }
+			if (!File.Exists(caminhoArquivo))
+			{
+				Console.WriteLine("Arquivo não encontrado.");
+				Console.ReadKey();
+				return;
+			}
 
-            try
-            {
-                Process.Start(new ProcessStartInfo
-                {
-                    FileName = caminhoArquivo,
-                    UseShellExecute = true
-                });
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Erro ao abrir o arquivo: {ex.Message}");
-                Console.ReadKey();
-            }
-        }
-    }
+			try
+			{
+				Process.Start(new ProcessStartInfo
+				{
+					FileName = caminhoArquivo,
+					UseShellExecute = true
+				});
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Erro ao abrir o arquivo: {ex.Message}");
+				Console.ReadKey();
+			}
+		}
+	}
 }
